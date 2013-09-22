ec6e057d13e45f57c409c3f5910d223f500fd31b
Vincent St-Amour <stamourv@racket-lang.org>
2013-09-22 16:46:29 -0400

    Interface "improvements".
    Pretty hacky.
    
    Need to convert other examples.

--- examples/funky-town.rkt
+++ examples/funky-town.rkt
@@ -2,30 +2,12 @@
 
 (require "../main.rkt")
 
-(emit (mix (list (sequence 1 (list (note 'C 5 1)
-                                   '(#f . 1)
-                                   (note 'C 5 1)
-                                   '(#f . 1)
-                                   (note 'A# 4 1)
-                                   '(#f . 1)
-                                   (note 'C 5 1)
-                                   '(#f . 3)
-                                   (note 'G 4 1)
-                                   '(#f . 3)
-
-                                   (note 'G 4 1)
-                                   '(#f . 1)
-                                   (note 'C 5 1)
-                                   '(#f . 1)
-                                   (note 'F 5 1)
-                                   '(#f . 1)
-                                   (note 'E 5 1)
-                                   '(#f . 1)
-                                   (note 'C 5 1)
-                                   '(#f . 9)
-                                   )
-                           380 sine-wave)
-                 1)
-           (list (drum 4 '(O #f #f #f X #f #f #f) 380)
-                 1))
-      "funky-town.wav")
+(emit
+ (mix
+  (sequence
+    sawtooth-wave #:bpm 380 #:times 2
+    [(C 5) #f (C 5) #f (A# 4) #f (C 5) (#f 3) (G 4) (#f 3)
+     (G 4) #f (C 5) #f (F 5) #f (E 5) #f (C 5) (#f 9)])
+  (drum #:bpm 380 #:times 8
+        (O #f #f #f X #f #f #f)))
+ "funky-town.wav")
